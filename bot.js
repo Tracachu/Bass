c52c23d2710c280eb4f41f99b070d15afea1d2d0
TracaPoo <42890509+Tracachu@users.noreply.github.com>
2018-10-23 20:18:20 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+const Music = require("discord.js-vibes");
+ 
+let bot = new Discord.Client();
+ 
+Music.start(bot, {
+    youtubeAPIKey: 'AIzaSyChV72AqgUOWab694WT3zdK6EIbY0EGRuc'
+  });
+
+  bot.on("ready", () => {
+      console.log("ready");
+  });
+ 
+
+bot.login("MzcxNjYwODI1Nzc1MzA4ODEw.DrEC7A.OQcDXgeQ4nu1hMtKZ0JdjGXsZFg")
